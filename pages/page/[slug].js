88b62be5740d159f6a6a69f2ecb791707871ee48
Qais Khalifa <109701506+qalassa@users.noreply.github.com>
2024-06-30 22:32:08 +0300

    Update [slug].js

--- pages/page/[slug].js
+++ pages/page/[slug].js
@@ -6,7 +6,7 @@ import Post from '@layouts/components/Post';
 import client from '@lib/contentful';
 import { sortByDate } from '@lib/utils/sortFunctions';
 const { blog_folder, pagination } = config.settings;
-import { getArticles ,getAllArticles } from '../lib/api';
+import { getArticles ,getAllArticles } from '../../lib/api';
 
 // blog pagination
 const BlogPagination = ({ posts, currentPage, pagination }) => {
