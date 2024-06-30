2b3d003ca518081aa36efa42e5e88a82657b446e
Qais Khalifa <109701506+qalassa@users.noreply.github.com>
2024-06-30 17:16:10 +0300

    Update [single].js

--- pages/posts/[single].js
+++ pages/posts/[single].js
@@ -1,5 +1,4 @@
-import { BLOCKS, INLINES } from '@contentful/rich-text-types';
-import client from '@lib/contentful';
+import { getArticle } from '@/lib/api';
 import PostSingle from '@layouts/PostSingle';
 
 const Article = ({ post, content, slug, posts }) => {
@@ -10,40 +9,32 @@ const Article = ({ post, content, slug, posts }) => {
 
 export default Article;
 
-// get post single slug
 export const getStaticPaths = async () => {
   const res = await client.getEntries({ content_type: 'article' });
-  const paths = res.items.map((item) => ({
-    params: { single: item.fields.slug.toString() }, // Ensure slug is a string
+  const paths = res.items.map(item => ({
+    params: { single: item.fields.slug.toString() },
   }));
 
-  return {
-    paths,
-    fallback: false,
-  };
+  return { paths, fallback: false };
 };
 
-// get post single content
 export const getStaticProps = async ({ params }) => {
   const { single } = params;
   try {
-    const postRes = await client.getEntries({
-      content_type: 'article',
-      'fields.slug': single,
-    });
+    const postRes = await getArticle(single);
 
-    if (!postRes.items.length) {
+    if (!postRes) {
       console.log(`No post found for slug: ${single}`);
       return { notFound: true };
     }
 
-    const post = postRes.items[0];
-    const content = post.fields.body;
+    const post = postRes;
+    const content = post.details.json;
 
     return {
       props: {
-        post: JSON.parse(JSON.stringify(post.fields)),
-        content: JSON.parse(JSON.stringify(content)),
+        post,
+        content,
         slug: single,
       },
     };
